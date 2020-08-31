f948092d01ea74169772ac4ce464b704238ec9e4
Cosmin Serdean <cosmin@cserdean.com>
2020-08-31 17:27:23 +0300

    feat: implement main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core';
+
+import ProfileWizard from './components/ProfileWizard';
+
+import theme from './utils/theme';
 
 function App() {
-  return <h1>Profile wizard</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <ProfileWizard />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/ProfileWizard/ProfileWizard.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import {
+  Container,
+  Typography,
+  makeStyles,
+  Card,
+  CardContent,
+  Step,
+  Stepper,
+  StepLabel,
+} from '@material-ui/core';
+
+import FirstStep from './components/FirstStep';
+
+type Step = 0 | 1 | 2;
+
+const STEPS = [
+  'Enter a name, email, password',
+  'Upload a profile photo and description',
+  'Summary',
+];
+
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    marginTop: theme.spacing(12),
+    marginBottom: theme.spacing(8),
+  },
+}));
+
+function getStep(step: Step) {
+  switch (step) {
+    case 0: {
+      return <FirstStep />;
+    }
+    case 1: {
+      return <h1>2nd</h1>;
+    }
+
+    case 1: {
+      return <h1>3rd</h1>;
+    }
+  }
+}
+
+const ProfileWizard = () => {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = useState<Step>(0);
+
+  return (
+    <Container fixed>
+      <Typography variant="h2" className={classes.heading}>
+        Create profile
+      </Typography>
+      <Card variant="outlined">
+        <CardContent>
+          <Stepper activeStep={activeStep} alternativeLabel>
+            {STEPS.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {getStep(activeStep)}
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
+
+export default ProfileWizard;
